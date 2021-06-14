7ac28403bd750d40911e7a672430470ea5df1520
Olivia Katelyn <oliviakatelyn@olivias-mbp.attlocal.net>
2021-06-14 12:55:32 -0700

    Code Checkbox functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [ buttonColor, setButtonColor ] = useState('red');
+  const [ disabled, setDisabled ] = useState(false);
+  
   const newButtonColor= buttonColor === 'red'? 'blue' : 'red';
 
   return (
     <div >
       <button 
-      style={{backgroundColor: buttonColor}}
-      onClick={() => setButtonColor(newButtonColor)}>
-        Change to {newButtonColor}</button>
+      style={{backgroundColor: buttonColor, color: 'white'}}
+      onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+        >Change to {newButtonColor}</button>
+        <input 
+        type='checkbox'
+        id='enable-button-checkbox'
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}/>
       </div>
   );
 }
